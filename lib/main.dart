a4f694b110629f190ccad3d621a1123e18ac6e31
krlaa <kevindurantony@gmail.com>
2022-10-31 21:15:11 -0400

    updated default transition and using Routes.x

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:munchease/utils/app_bindings.dart';
+import 'package:munchease/utils/app_boxes.dart';
 import 'package:munchease/utils/theme_utils.dart';
-
 import 'utils/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await MunchBox.openBoxes();
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+        defaultTransition: Transition.cupertino,
         themeMode: ThemeMode.dark,
         theme: MunchTheme.lightTheme,
         darkTheme: MunchTheme.darkTheme,
@@ -21,7 +27,7 @@ class MainApp extends StatelessWidget {
         initialBinding: AppBinding(),
         // The initial route this can be changed to the splash screen route as well.
         // TODO:change back to home route
-        initialRoute: '/cuisine',
+        initialRoute: Routes.SPLASH,
         // Predefined pages.
         getPages: AppPages.pages);
   }
