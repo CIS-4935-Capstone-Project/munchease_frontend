eeb6b297a453c271daab679f1673d435eff91099
jet1337 <jacob.e.taylor20@gmail.com>
2022-11-08 22:18:31 -0500

    fixing signin screen

--- lib/utils/login_provider.dart
+++ lib/utils/login_provider.dart
@@ -1,9 +1,24 @@
 import 'package:get/get.dart';
 
 class LoginProvider extends GetConnect {
-  Future<Response> registerUser(Map data) => post('registerurl', data);
+  Future<Response> registerUser(Map data) async {
+    final response = await post('registerurl', data);
+    if (response.status.hasError) {
+      return Future.error(Exception(response.statusText));
+    } else {
+      return response.body;
+    }
+  }
 
   Future<Response> signIn(Map data) => post('signinURL', data);
 
-  Future<Response> getBeer() => get('https://random-data-api.com/api/v2/beer');
+  // Future<Response> getBeer() async {
+  //   await get('https://random-data-api.com/api/v2/beer').then((response) {
+  //     if (response.status.hasError) {
+  //       return Future.error(Exception(response.statusText));
+  //     } else {
+  //       return response.body;
+  //     }
+  //   });
+  // }
 }
