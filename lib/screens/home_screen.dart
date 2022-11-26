b7feec7bd805ea92fa304a6d8a51f69e1599c8bf
jet1337 <jacob.e.taylor20@gmail.com>
2022-11-25 21:28:03 -0500

    hambgurger

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:munchease/controllers/home_screen_controller.dart';
 import 'package:munchease/controllers/splash_screen_controller.dart';
+import 'package:munchease/widgets/me_drawer.dart';
 import 'package:munchease/widgets/me_text_button.dart';
 import 'package:rive/rive.dart';
 
@@ -11,10 +12,13 @@ class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
   final controller = Get.put(HomeScreenController());
   SplashScreenController splashController = Get.find();
+  final GlobalKey<ScaffoldState> _key = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _key,
+      drawer: const MunchDrawer(),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
@@ -28,71 +32,73 @@ class HomeScreen extends StatelessWidget {
                     child: Padding(
                       padding: const EdgeInsets.all(15.0),
                       child: GestureDetector(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(3.0),
-                              child: Container(
-                                height: 4,
-                                width: 30,
+                        child: SizedBox(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(3.0),
+                                child: Container(
+                                  height: 4,
+                                  width: 30,
+                                  decoration: const BoxDecoration(
+                                    color: Color.fromARGB(255, 226, 176, 140),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10.0)),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                height: 2,
+                                width: 35,
+                                decoration: BoxDecoration(
+                                  color: Colors.yellow[600]!,
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(10.0)),
+                                ),
+                              ),
+                              Container(
+                                height: 5,
+                                width: 37,
                                 decoration: const BoxDecoration(
-                                  color: Color.fromARGB(255, 226, 176, 140),
+                                  color: Color.fromARGB(255, 255, 68, 68),
                                   borderRadius:
                                       BorderRadius.all(Radius.circular(10.0)),
                                 ),
                               ),
-                            ),
-                            Container(
-                              height: 2,
-                              width: 35,
-                              decoration: BoxDecoration(
-                                color: Colors.yellow[600]!,
-                                borderRadius: const BorderRadius.all(
-                                    Radius.circular(10.0)),
-                              ),
-                            ),
-                            Container(
-                              height: 5,
-                              width: 37,
-                              decoration: const BoxDecoration(
-                                color: Color.fromARGB(255, 255, 68, 68),
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10.0)),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(3.0),
-                              child: Container(
-                                height: 4,
-                                width: 30,
-                                decoration: const BoxDecoration(
-                                  color: Color.fromARGB(255, 226, 176, 140),
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(10.0)),
+                              Padding(
+                                padding: const EdgeInsets.all(3.0),
+                                child: Container(
+                                  height: 4,
+                                  width: 30,
+                                  decoration: const BoxDecoration(
+                                    color: Color.fromARGB(255, 226, 176, 140),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10.0)),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                        onTap: () => {
-                          Get.defaultDialog(
-                            title: 'Test',
-                            onCancel: () => Get.back(),
-                          )
+                        onTap: () {
+                          _key.currentState!.openDrawer();
                         },
                         onTapCancel: () => {},
                       ),
                     ),
                   ),
-                  SizedBox(
-                    // logo
-                    width: 51,
-                    height: 51,
-                    child: Hero(
-                      tag: 'logo',
-                      child: Rive(
-                        artboard: splashController.birdArtboard,
+                  Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: SizedBox(
+                      // logo
+                      width: 51,
+                      height: 51,
+                      child: Hero(
+                        tag: 'logo',
+                        child: Rive(
+                          artboard: splashController.birdArtboard,
+                        ),
                       ),
                     ),
                   ),
